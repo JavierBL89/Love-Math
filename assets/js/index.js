d2a1480305eec0bf59e07481cc2a8ce181e4ff82
JavierBL89 <notri80@gmail.com>
2021-10-18 22:53:54 +0000

    running game

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,33 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+
+let buttons = document.getElementsByTagName("button");
+for(let button of buttons){
+    button.addEventListener("click", function(){
+        if(this.getAttribute("data-type")=== "submit"){
+            alert("puta");
+        }else{
+            let gameType = this.getAttribute("data-type");
+            runGame(gameType);
+        }
+    })
+    runGame("addition");
+};
+
+
+/**
+ * The main game "loop", called whan the script is first loade
+ * and after the user's answer has ben procceded
+ */
 function runGame(){
 
+  let num1 = Math.floor(Math.random() *25) + 1;
+  let num2 = Math.floor(Math.random() *25) + 1;
+
+  document.getElementsByClassName("operand1").innerHTML = num1;
+  document.getElementsByClassName("operand2").innerHTML = num2;
 }
+
 function checkAnswer(){
     
 }
@@ -26,3 +53,4 @@ function displayMultiplyQuestion(){
 function displayDivideQuestion(){
     
 }
+
