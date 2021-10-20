2fd376d740147272b0f17dfc2ab4b82eac3329d3
JavierBL89 <notri80@gmail.com>
2021-10-20 11:33:13 +0000

    all functions workin

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,7 +28,16 @@ function runGame(gameType){
   if(gameType === "addition"){
       displayAdditionQuestion(num1, num2);
       calculateCorrectAnswer();
-  }else{
+  }else if(gameType === "multiply"){
+      displayMultiplyQuestion(num1, num2);
+      calculateCorrectAnswer();
+  }else if(gameType === "divide"){
+    displayDivideQuestion(num1, num2);
+    calculateCorrectAnswer();
+}else if(gameType === "substract"){
+displaySubtractQuestion(num1, num2);
+calculateCorrectAnswer();
+}else{
       alert(`Unknown game type: ${gameType}`);
       throw `Unknown game type: ${gameType}. Aborting!`;
   }
@@ -56,9 +65,10 @@ function checkAnswer(){
     let isCorrect = userAnswer === result[0];
     if(isCorrect){
         alert("congartulations," + userAnswer + " is equal to" + result[0]);
-
+        incrementScore();
     }else{
         alert(`bad luck, your answer "${userAnswer}" is not correct, the correct answer is ${result[0]}`);
+        incrementWrongAnswer();
     }
     // document.getElementById("answer-box").innerHTML =reset();
     runGame(result[1]);
@@ -84,6 +94,12 @@ function calculateCorrectAnswer(){
 
         return [operand1 + operand2, "addition"];
         
+    }else if(operator === "x"){
+        return [operand1 * operand2, "multiply"];
+    }else if(operator == "-"){
+        return [operand1 - opeerand2, "subtract"]
+    }else if(operator == "/"){
+        return [operand1 / operand2, "divide"]
     }else{
         alert(`Unimplemented operator: ${operator}`);
         throw `Unimplemented operator: ${operator}`;
@@ -91,26 +107,59 @@ function calculateCorrectAnswer(){
     
 }
 
+/**
+ * Gets current score from the DOM and increments it by 1
+ */
 function incrementScore(){
-    
+    let currentScore = parseInt(document.getElementById("score").innerText);
+
+    // innerText NUMBERS CAN'T BE TARGGERED SO
+    // WE NEED TO GET THE HTML AGAIN AND RESET IT AS A NUMBER 
+    document.getElementById("score").innerText = ++currentScore;
 }
+
+
+/**
+ * Gets the current incorrect score from the DOM adn increments it by 1
+ */
 function incrementWrongAnswer(){
-    
+    let currentScore = parseInt(document.getElementById("incorrect").innerText);
+
+    // innerText NUMBERS CAN'T BE TARGGERED SO
+    // WE NEED TO GET THE HTML AGAIN AND RESET IT AS A NUMBER 
+    document.getElementById("incorrect").innerText = ++currentScore;
 }
+
+
 function displayAdditionQuestion(operand1, operand2){
     
     document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
   document.getElementById("operator").textContent = "+" ;
 }
-function displaySubtractQuestion(){
-    
+
+
+function displaySubtractQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "-" ;
+
 }
-function displayMultiplyQuestion(){
+
+/**
+ * Gets the random numbers and populate the into DOM
+ */
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x" ;
+
     
 }
 
-function displayDivideQuestion(){
-    
+function displayDivideQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/" ;
 }
 
